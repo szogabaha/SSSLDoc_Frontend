e2d97e680645963c85ec906d30fb688688f3268c
Elo Zsombor <elozsombor@gmail.com>
2021-05-13 20:32:06 +0200

    add tickets-listing features

--- kot_nhf/my-app/src/app/tickets/tickets.component.ts
+++ kot_nhf/my-app/src/app/tickets/tickets.component.ts
@@ -4,6 +4,10 @@ import { LoginService } from '../../services/login/login.service'
 import { UserTicketService } from '../../services/userTicketService/user-ticket.service'
 import { Location } from '@angular/common';
 import { CreateNewTicketRequest } from '../../model/CreateNewTicketRequest'
+import { RegisteredByMe, RegisteredByMeRequest, Status } from 'src/model/RegisteredByMe';
+import { Observable } from 'rxjs';
+import { TicketDetail } from 'src/model/TicketDetail';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-tickets',
@@ -25,32 +29,122 @@ export class TicketsComponent implements OnInit {
         this.loginService.getBackendJwt(schAuthAccessToken)
         this.location.replaceState(this.location.path().split('?')[0], '');
 
-        //TODO ez csak egy példa arra, hogy hogyan kell használni a service-t.
-        let myTickets = this.userTicketService.getMyTickets().subscribe({
-          next: data => {
-            let list = data.registeredByMe
-            console.log(list)
-            return list
-          },
-          error: error => {
-            console.log("ERROR")
-          }            
-        })
 
         this.userTicketService.getTicketByUuid("7e1b2d9a-c379-4871-aa6b-c038f3681a82").subscribe(ticket => {
-          console.log(ticket)
+          console.log(ticket);
         })
-        ///Ezt kikommenteztem, hogy ne szemetelje szét a db-t, de egyébként itt egy példa új elem felvitelére.
-        /*let request : CreateNewTicketRequest = {
-          ticketType: "feedback-request",
-          isAnonym: true,
-          description: "negyedik requests"
-        }
-        this.userTicketService.createNewTickets(request).subscribe();
-        */
+        this.filter = Status.All;
       }
+      ///Ezt kikommenteztem, hogy ne szemetelje szét a db-t, de egyébként itt egy példa új elem felvitelére.
+      /*let request : CreateNewTicketRequest = {
+        ticketType: "misc",
+        isAnonym: true,
+        description: "valahanyadik"
+      }
+      this.userTicketService.createNewTickets(request).subscribe();*/
+      this.getTickets();
       
     })  
   }
 
+  getTickets() {
+    this.userTicketService.getMyTickets().subscribe({
+      next: data => {
+        this.myregisteredtickets = data.registeredByMe.filter(cmp => {
+          if (this.filter == Status.All){
+            return true;
+          }
+          if (this.filter == Status.New){
+            return cmp.assignedTo == null && cmp.isActive && cmp.ticketType != "feedback-request"
+          }
+          if (this.filter == Status.Assigned){
+            return cmp.assignedTo != null && cmp.isActive && cmp.ticketType != "feedback-request"
+          }
+          if (this.filter == Status.Feedback){
+            return cmp.ticketType == "feedback-request"
+          }
+          if (this.filter == Status.Closed){
+            return !cmp.isActive && cmp.ticketType != "feedback-request"
+          }
+          return true;
+        });
+        this.assignedtickets = data.assignedToMe;
+        console.log(this.myregisteredtickets);
+        console.log(this.assignedtickets);
+      },
+      error: error => {
+        console.log("ERROR");
+      }            
+    })
+  }
+
+  getType(ticket : RegisteredByMe) : string{
+    let result = "";
+    switch(ticket.ticketType){
+      case "feedback-request":
+        result = "Feedback";
+        break;
+      case "criticism":
+        result = "Criticism";
+        break;
+      case "advice-request":
+        result = "Advice";
+        break;
+      case "misc":
+        result = "Misc";
+        break;
+    }
+    return result;
+  }
+
+  getCreatedBy(ticket : RegisteredByMe) : string{
+    if (ticket.createdBy == null){
+      return "Anonymous";
+    } else{
+      return ticket.createdBy;
+    }
+  }
+
+  getActive(ticket : RegisteredByMe) : string{
+    if (ticket.isActive){
+      return "Active";
+    } else {
+      return "Closed"
+    }
+  }
+
+  getCreatedAt(ticket : RegisteredByMe) : string{
+    let date = new Date(ticket.createdAt);
+    return date.toLocaleString();
+  }
+
+  getAssignee(ticket: RegisteredByMe) : string{
+    if (ticket.assignedTo == null){
+      return "None";
+    } else {
+      return ticket.assignedTo;
+    }
+  }
+
+  getStyle(ticket : RegisteredByMe) : string{
+    if(ticket.ticketType == "feedback-request")
+      return "table-warning";
+    if(!ticket.isActive)
+      return "table-dark";
+    if(ticket.assignedTo == null)
+      return "table-info";
+    else
+      return "table-secondary";
+  }
+
+  reFilter(filtering : Status){
+      this.filter = filtering;
+      this.getTickets();
+  }
+
+  myregisteredtickets : RegisteredByMe[] | undefined;
+  //TODO: ha meglesz az ügyintézős / admin cucc, akkor kell majd vagy majd megkérdezem
+  assignedtickets : RegisteredByMe[] | undefined;
+  filter : Status = Status.All;
+  public status = Status;
 }
--- kot_nhf/my-app/src/model/RegisteredByMe.ts
+++ kot_nhf/my-app/src/model/RegisteredByMe.ts
@@ -1,20 +1,26 @@
+export interface RegisteredByMe {
+    ticketId: string;
+    createdAt: number;
+    createdBy: string;
+    ticketType: string;
+    assignedTo?: any;
+    isActive: boolean;
+    unansweredMessages: any[];
+    unreviewedMessages: any[];
+}
 
+export interface RegisteredByMeRequest {
+    registeredByMe: RegisteredByMe[];
+    assignedToMe: RegisteredByMe[];
+}
 
-    export interface RegisteredByMe {
-        ticketId: string;
-        createdAt: number;
-        createdBy: string;
-        ticketType: string;
-        assignedTo?: any;
-        isActive: boolean;
-        unansweredMessages: any[];
-        unreviewedMessages: any[];
-    }
-
-    export interface RegisteredByMeRequest {
-        registeredByMe: RegisteredByMe[];
-        assignedToMe: any[];
-    }
+export enum Status {
+    All = "All",
+    New = "New",
+    Assigned = "Assigned",
+    Feedback = "Feedback",
+    Closed = "Closed"
+}
 
 
 
